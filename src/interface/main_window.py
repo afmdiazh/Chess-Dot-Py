6783b74f7a2769d93200594864707c92b27c45a7
MD <md@localhost>
2022-04-21 13:22:13 +0200

    Added tabWidget with two sections

--- src/interface/main_window.py
+++ src/interface/main_window.py
@@ -17,6 +17,15 @@ class Ui_MainWindow(object):
         MainWindow.resize(800, 600)
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
+        self.tabWidget = QtWidgets.QTabWidget(self.centralwidget)
+        self.tabWidget.setGeometry(QtCore.QRect(10, 10, 771, 531))
+        self.tabWidget.setObjectName("tabWidget")
+        self.qWidget_Player = QtWidgets.QWidget()
+        self.qWidget_Player.setObjectName("qWidget_Player")
+        self.tabWidget.addTab(self.qWidget_Player, "")
+        self.qWidget_Leaderboard = QtWidgets.QWidget()
+        self.qWidget_Leaderboard.setObjectName("qWidget_Leaderboard")
+        self.tabWidget.addTab(self.qWidget_Leaderboard, "")
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QtWidgets.QMenuBar(MainWindow)
         self.menubar.setGeometry(QtCore.QRect(0, 0, 800, 21))
@@ -27,11 +36,14 @@ class Ui_MainWindow(object):
         MainWindow.setStatusBar(self.statusbar)
 
         self.retranslateUi(MainWindow)
+        self.tabWidget.setCurrentIndex(0)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.tabWidget.setTabText(self.tabWidget.indexOf(self.qWidget_Player), _translate("MainWindow", "Player"))
+        self.tabWidget.setTabText(self.tabWidget.indexOf(self.qWidget_Leaderboard), _translate("MainWindow", "Leaderboard"))
 
 
 if __name__ == "__main__":
