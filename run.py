2730f81f052d7d48f4265f3bc8dbbeccf5800d7a
MD <md@localhost>
2022-04-21 15:28:14 +0200

    Basic window skeleton, implemented button connection

--- run.py
+++ run.py
@@ -1,11 +1,9 @@
-from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5 import QtWidgets
 from src.main import Main
 
 if __name__ == "__main__":
     import sys
     app = QtWidgets.QApplication(sys.argv)
-    MainWindow = QtWidgets.QMainWindow()
-    ui = Main()
-    ui.setupUi(MainWindow)
-    MainWindow.show()
+    window = QtWidgets.QMainWindow()
+    ui = Main(window)
     sys.exit(app.exec_())
--- src/main.py
+++ src/main.py
@@ -1,6 +1,38 @@
 from .interface.main_window import Ui_MainWindow
+from .data import get_player, get_leaderboard
 
 
 class Main(Ui_MainWindow):
-    def __init__(self):
+    """
+    Chess-Dot-Py main class
+    """
+
+    def __init__(self, window):
+        """
+        Initializes the window
+        """
         super().__init__()
+        self.setupUi(window)
+        window.show()
+        self.set_connections()
+
+    def set_connections(self):
+        """
+        Connects UI elements to functions
+        """
+        self.pushButtonPlayerSearch.clicked.connect(self.button_search_clicked)
+
+    def button_search_clicked(self):
+        """
+        Executed when pushButtonPlayerSearch is clicked
+        """
+        button_text = self.lineEditPlayerSearch.text().strip()
+        self.update_player_tab(button_text)
+
+    def update_player_tab(self, player_name):
+        """
+        Updates player tab
+        """
+        player = get_player(player_name)
+        if player:
+            player.print_basic_info()
